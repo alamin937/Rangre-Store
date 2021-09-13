2b6fbe8be7e620d089b7121bc4b10ff16f985a34
alamin937 <mdalamin76812@gmail.com>
2021-09-13 14:22:09 +0600

    header add

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h5>${product.title}</h5>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
